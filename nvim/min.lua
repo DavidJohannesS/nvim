199674e81d6e1d61018d5f0897155f4ebbb8b3ba
meow <davidjohannesSD@gmail.com>
2025-03-27 23:34:07 +0100

    show error shortcut

---
+++ nvim/min.lua
@@ -0,0 +1,6 @@
+
+vim.cmd([[
+    let g:vimwiki_list = [{'path': '/home/meow/Nextcloud/Notes/', 'syntax': 'markdown', 'ext': '.md'}]
+    let g:vimwiki_global_ext = 0
+    let g:vimwiki_ext = '.md'
+]])
