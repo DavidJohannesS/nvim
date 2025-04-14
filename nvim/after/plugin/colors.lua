dbe606bb4e8d6cd201b05eeb1c7688456eec7c94
meow <davidjohannesSD@gmail.com>
2025-04-14 23:31:05 +0200

    Updated color scheme

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -3,10 +3,15 @@ function ColorMeow(color)
     color = color or "rose-pine"
     vim.cmd.colorscheme(color)
     -- Uncomment these if you want transparent backgrounds
-     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
--- Call the color function first
 ColorMeow()
 
+vim.api.nvim_set_hl(0, "NERDTreeCWD", { fg = "#98c379", bold = true })
+vim.api.nvim_set_hl(0, "NERDTreeDir", { fg = "#7B4D66" })
+vim.api.nvim_set_hl(0, "NERDTreeDirSlash", { fg = "#98c379" })
+vim.api.nvim_set_hl(0, "NERDTreeFile", { fg = "#ffffff" })
+vim.api.nvim_set_hl(0, "NERDTreeExecFile", { fg = "#ebbcba" })
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#7b4d66", bg = "none" })
