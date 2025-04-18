564ad686f18974c198375c097d979814224d869f
DavidJohannesS <davidjohannesSD@gmail.com>
2025-04-18 21:36:33 +0200

    Added markdown to treesitter

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 
 require('nvim-treesitter.configs').setup({
-    ensure_installed = { "lua", "dockerfile", "helm", "yaml", "java", "bash" }, -- Specify the parsers
+    ensure_installed = { "lua", "dockerfile", "markdown", "helm", "yaml", "java", "bash" }, -- Specify the parsers
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = true, -- Also enable Vim's regex-based highlighting
