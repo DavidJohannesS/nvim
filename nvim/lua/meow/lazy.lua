72e0da7d967acf40ad4bb51ceb198827c37fe397
DavidJohannesS <davidjohannesSD@gmail.com>
2026-03-26 21:45:30 +0100

    Adding harpoon

--- nvim/lua/meow/lazy.lua
+++ nvim/lua/meow/lazy.lua
@@ -143,5 +143,31 @@ require("lazy").setup({
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
   opts = {}
+},
+{
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local harpoon = require("harpoon")
+
+    harpoon:setup()
+
+    -- Keymaps
+    vim.keymap.set("n", "<leader>a", function()
+      harpoon:list():add()
+    end, { desc = "Harpoon add file" })
+
+    vim.keymap.set("n", "<leader>h", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = "Harpoon menu" })
+
+    -- Direct file jumps
+    vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<leader>5", function() harpoon:list():select(4) end)
+  end
 }
 })
